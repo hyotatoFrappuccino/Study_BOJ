16ff9a2100fdce987c24ddf74e2964a0a83edd06
hyojae0812 <hjkiln@naver.com>
2023-05-28 23:21:03 +0900

    백준 15894번 풀이

---
+++ 10000/P15894.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P15894 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(4*n);
+    }
+}
