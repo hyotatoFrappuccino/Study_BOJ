46e72d7184680f0f41342f9c0f84ff139e8aa26b
hyotatoFrappuccino <hjkiln0@gmail.com>
2023-11-02 12:20:52 +0900

    .

---
+++ 20000/AI_HW_04.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AI_HW_04 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+    }
+}
