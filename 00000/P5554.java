4f61ab3c2b10bd548d5d86de516b9143294bc923
hyotatoFrappuccino <hjkiln0@gmail.com>
2024-05-27 01:04:42 +0900

    심부름 가는 길

---
+++ 00000/P5554.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P5554 {
+    static StringBuilder output;
+    static int sum;
+
+    static void input(String[] lines) {
+        for (int i = 0; i < 4; i++) {
+            sum += Integer.parseInt(lines[i]);
+        }
+        output = new StringBuilder();
+    }
+
+    static String process() {
+        output.append(sum / 60).append("\n");
+        output.append(sum % 60);
+        return output.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        input(readLines());
+        System.out.println(process());
+    }
+
+    private static String[] readLines() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            lines.add(line);
+        }
+        br.close();
+
+        String[] linesArray = new String[lines.size()];
+        linesArray = lines.toArray(linesArray);
+        return linesArray;
+    }
+}
