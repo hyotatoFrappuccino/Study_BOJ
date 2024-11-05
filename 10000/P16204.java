f7e824a5f19bc26dcc1ec97e20fa0be32dd0ae31
hyotatoFrappuccino <hjkiln0@gmail.com>
2024-11-06 00:03:17 +0900

    카드 뽑기

---
+++ 10000/P16204.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P16204 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        bw.append(String.valueOf(Math.min(M, K) + Math.min(N - M, N - K)));
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
